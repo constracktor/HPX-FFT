c7d4428a959482351be4996c5aa9da1f60985c3e
constracktor <strack.as@icloud.com>
2025-09-09 10:36:49 -0500

    Minor cleanup

--- compile_hpxfft_examples.sh
+++ compile_hpxfft_examples.sh
@@ -8,8 +8,5 @@ cd examples/hpxfft
 BUILD_DIR=build
 CMAKE_COMMAND=cmake
 rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR
-#$LDFLAGS=$LDFLAGS 
 $CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release -DHPXFFT_DIR=../../install/lib64/cmake/HPXFFT
-
 make -j
-#make VERBOSE=1 -j $(grep -c ^processor /proc/cpuinfo)
