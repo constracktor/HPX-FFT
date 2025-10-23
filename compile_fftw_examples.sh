ff642b5ac3d177a37ebf8134a16389212841c7de
constracktor <strack.as@icloud.com>
2025-10-23 17:54:55 +0200

    Add spack environment for simcl1

--- compile_fftw_examples.sh
+++ compile_fftw_examples.sh
@@ -1,12 +1,41 @@
-module load gcc/14.2.0
-spack load fftw%gcc@14.2.0
+#!/usr/bin/bash
+#!/usr/bin/bash
+################################################################################
+# Config
+################################################################################
+if command -v spack &> /dev/null; then
+    echo "Spack command found, checking for environments..."
+
+    # Get current hostname
+    HOSTNAME=$(hostname -s)
+
+    if [[ "$HOSTNAME" == "ipvs-epyc1" ]]; then
+        echo "tbd."
+    elif [[ "$HOSTNAME" == "sven0"  ||  "$HOSTNAME" == "sven1" ]]; then
+        echo "tbd."
+    elif [[ $(uname -i) == "aarch64" ]]; then
+        echo "tbd."
+    elif [[ "$HOSTNAME" == "simcl1n1" || "$HOSTNAME" == "simcl1n2" ]]; then
+        # Check if the spack environment exists
+        SPACK_ENV=hpxfft_x86_simcl
+        if spack env list | grep -q "${SPACK_ENV}"; then
+            echo "Found ${SPACK_ENV} environment, activating it."
+            module load gcc/14.1.0
+            spack env activate ${SPACK_ENV}
+	    # FFTW paths
+	    export FFTW_DIR="$(spack -i location fftw)/lib"
+	    export PKG_CONFIG_PATH="$FFTW_DIR/pkgconfig":$PKG_CONFIG_PATH
+        fi
+    else
+        echo "Hostname is $HOSTNAME — no action taken."
+    fi
+else
+    echo "Spack command not found. Building example without Spack."
+    # Assuming that Spack is not required on given system
+fi
 ################################################################################
 # Compilation
 ################################################################################
-# FFTW paths
-export FFTW_DIR="$(spack -i location fftw)/lib"
-export PKG_CONFIG_PATH="$FFTW_DIR/pkgconfig":$PKG_CONFIG_PATH
-
 cd examples/fftw
 BUILD_DIR=build
 CMAKE_COMMAND=cmake
@@ -14,3 +43,5 @@ rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR
 $CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release
 make -j
 
+mkdir -p $BUILD_DIR/result/runtimes
+mkdir -p $BUILD_DIR/result/plans
--- compile_hpxfft_examples.sh
+++ compile_hpxfft_examples.sh
@@ -1,6 +1,35 @@
-module load gcc/14.2.0
-spack load hpx%gcc@14.2.0
-spack load fftw%gcc@14.2.0
+#!/usr/bin/bash
+################################################################################
+# Config
+################################################################################
+if command -v spack &> /dev/null; then
+    echo "Spack command found, checking for environments..."
+
+    # Get current hostname
+    HOSTNAME=$(hostname -s)
+
+    if [[ "$HOSTNAME" == "ipvs-epyc1" ]]; then
+        echo "tbd."
+    elif [[ "$HOSTNAME" == "sven0"  ||  "$HOSTNAME" == "sven1" ]]; then
+        echo "tbd."
+    elif [[ $(uname -i) == "aarch64" ]]; then
+        echo "tbd."
+    elif [[ "$HOSTNAME" == "simcl1n1" || "$HOSTNAME" == "simcl1n2" ]]; then
+        # Check if the spack environment exists
+        SPACK_ENV=hpxfft_x86_simcl
+        if spack env list | grep -q "${SPACK_ENV}"; then
+            echo "Found ${SPACK_ENV} environment, activating it."
+            module load gcc/14.1.0
+            spack env activate ${SPACK_ENV}
+	    LIB_DIR=lib
+        fi
+    else
+        echo "Hostname is $HOSTNAME — no action taken."
+    fi
+else
+    echo "Spack command not found. Building example without Spack."
+    # Assuming that Spack is not required on given system
+fi
 ################################################################################
 # Compilation
 ################################################################################
@@ -8,5 +37,5 @@ cd examples/hpxfft
 BUILD_DIR=build
 CMAKE_COMMAND=cmake
 rm -rf $BUILD_DIR && mkdir $BUILD_DIR && cd $BUILD_DIR
-$CMAKE_COMMAND .. -DCMAKE_BUILD_TYPE=Release -DHPXFFT_DIR=../../install/lib64/cmake/HPXFFT
+$CMAKE_COMMAND .. -Wno-dev -DCMAKE_BUILD_TYPE=Release -DHPXFFT_DIR=../../install/$LIB_DIR/cmake/HPXFFT
 make -j
