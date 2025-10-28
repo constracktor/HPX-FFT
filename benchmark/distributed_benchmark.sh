94211e8f65b86b5f92a03b94c633b387dc79c7b6
constracktor <strack.as@icloud.com>
2025-10-28 15:22:36 -0400

    Add distributed benchmark scripts

--- benchmark/distributed_benchmark.sh
+++ benchmark/distributed_benchmark.sh
@@ -1,67 +1,127 @@
-#!/bin/bash
+#!/usr/bin/bash
+# Benchmark script for shared memory 
+# $1: FFTW planning flag (estimate/measure)
+# $2: Collective (scatter/all_to_all)
 ################################################################################
-# Benchmark script for distributed memory 
-# $1: partition (buran/medusa with mpi/lci/tcp/shmem)
-# $2 communication scheme (scatter/all_to_all)
-if [[ "$1" == "buran_mpi" ]]
-then
-    # 16 nodes available
-    module load llvm/17.0.1
-    module load openmpi
-    PARTITION=buran
+# Config
+################################################################################
+TOP_DIR=$(pwd)
+EXAMPLES_DIR=$TOP_DIR/examples
+
+# HPX-FFT implementations
+HPXFFT_EXECUTABLES=(
+    "$EXAMPLES_DIR/hpxfft/build/hpxfft_distributed_loop"
+    "$EXAMPLES_DIR/hpxfft/build/hpxfft_distributed_agas"
+)
+# FFTW implementations
+FFTW_EXECUTABLES=(
+    "$EXAMPLES_DIR/fftw/build/fftw_mpi_omp"
+    "$EXAMPLES_DIR/fftw/build/fftw_mpi_threads"
+)
+
+# Get current hostname
+HOSTNAME=$(hostname -s)
+if [[ "$HOSTNAME" == "ipvsmisc" ]]; then
+    echo "tbd."
+elif [[ "$HOSTNAME" == "rostam" ]]; then
+    echo "tbd."
+elif [[ "$HOSTNAME" == "login1" ]]; then
+    PARTITION=short
+    NODES_POW=1
     THREADS=48
-    NODES_POW=4
-elif [[ "$1" == "buran_lci" ]]
-then
-    # 16 nodes available
-    module load llvm/17.0.1
-    PARTITION=buran
-    THREADS=48
-    NODES_POW=4
-    export LD_LIBRARY_PATH=/home/alex/hpxsc_installations/hpx_1.9_lci_clang_17.0.1/install/lib64:$LD_LIBRARY_PATH
-elif [[ "$1" == "buran_tcp" ]] || [[ "$1" == "buran_shmem" ]]
-then
-    # 16 nodes available
-    module load llvm/17.0.1
-    PARTITION=buran
-    THREADS=48
-    NODES_POW=4
-elif [[ "$1" == "medusa_mpi" ]]
-then
-    # 14 nodes available
-    module load llvm/17.0.1
-    module load openmpi
-    PARTITION=medusa
-    THREADS=40
-    NODES_POW=3
-elif [[ "$1" == "medusa_lci" ]]
-then
-    # 14 nodes available
-    module load llvm/17.0.1
-    PARTITION=medusa
-    THREADS=40
-    NODES_POW=3
-    export LD_LIBRARY_PATH=/home/alex/hpxsc_installations/hpx_1.9_lci_clang_17.0.1_medusa/install/lib64:$LD_LIBRARY_PATH
-elif [[ "$1" == "medusa_tcp" ]] || [[ "$1" == "medusa_shmem" ]]
-then
-    # 14 nodes available
-    module load llvm/17.0.1
-    PARTITION=medusa
-    THREADS=40
-    NODES_POW=3
+elif [[ "$HOSTNAME" == "simcl1" ]]; then
+    echo "tbd."
 else
-  echo 'Please specify partition and parcelport'
-  exit 1
+    echo "Hostname is $HOSTNAME — no action taken."
+    exit 1
 fi
-LOOP=50
-FFTW_PLAN=measure
-NODES=$((2**$NODES_POW))
+NODES=$((2**NODES_POW))
+
+# Set FFTW planning
+FFTW_PLAN=$1
+# Check if FFTW_PLAN was provided
+if [ -z "$FFTW_PLAN" ]; then
+    echo "Error: FFTW_PLAN parameter not set."
+    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all"
+    exit 1
+fi
+# Set Collective
 COLLECTIVE=$2
-BUILD_DIR=../build_$1
-cd sbatch_scripts
-# HPX implementations
-sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS run_hpx_dist.sh $BUILD_DIR/fft_hpx_loop $FFTW_PLAN $NODES_POW $COLLECTIVE $LOOP
-#sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS run_hpx_dist.sh $BUILD_DIR/fft_hpx_task_agas $FFTW_PLAN $NODES_POW $COLLECTIVE $LOOP
-# FFTW backends
-#sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS run_fftw_dist.sh $BUILD_DIR/fftw_mpi_threads $FFTW_PLAN $NODES $THREADS $LOOP
-#sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS run_fftw_dist.sh $BUILD_DIR/fftw_mpi_omp $FFTW_PLAN $NODES $THREADS $LOOP
+# Check if COLLECITVE was provided
+if [ -z "$COLLECTIVE" ]; then
+    echo "Error: COLLECTIVE parameter not set."
+    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all"
+    exit 1
+fi
+
+################################################################################
+# Run benchmarks
+################################################################################
+RESULT_DIR=$TOP_DIR/distributed_benchmark_on_$HOSTNAME
+SCRIPT_DIR=$TOP_DIR/benchmark/sbatch_scripts
+
+################################################################################
+# Strong scaling
+# Directories
+mkdir -p $RESULT_DIR/strong_scaling/runtimes
+mkdir -p $RESULT_DIR/strong_scaling/plans
+cd $RESULT_DIR/strong_scaling
+
+# Config
+LOOP=2
+BASE_SIZE=16384
+START_NODES_POW=0
+
+# Loop over HPX-FFT executables
+RUN_SCRIPT="$SCRIPT_DIR/run_hpxfft_strong_distributed.sh"
+for EXE in "${HPXFFT_EXECUTABLES[@]}"; do
+  echo "Submitting job for executable: $EXE"
+
+  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
+    $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP
+done
+
+# Loop over FFTW executables
+RUN_SCRIPT="$SCRIPT_DIR/run_fftw_strong_distributed.sh"
+# Loop over executables
+for EXE in "${FFTW_EXECUTABLES[@]}"; do
+  echo "Submitting job for executable: $EXE"
+
+  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
+    $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP
+done
+
+################################################################################
+# Weak scaling
+# Directories
+mkdir -p $RESULT_DIR/weak_scaling/runtimes
+mkdir -p $RESULT_DIR/weak_scaling/plans
+cd $RESULT_DIR/weak_scaling
+
+# Config
+LOOP=2
+BASE_SIZE=512
+START_THREAD_POW=0
+
+# Loop over HPX-FFT executables
+RUN_SCRIPT="$SCRIPT_DIR/run_hpxfft_weak_distributed.sh"
+for EXE in "${HPXFFT_EXECUTABLES[@]}"; do
+  echo "Submitting job for executable: $EXE"
+
+  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
+    $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP
+done
+
+# Loop over FFTW executables
+RUN_SCRIPT="$SCRIPT_DIR/run_fftw_weak_distributed.sh"
+# Loop over executables
+for EXE in "${FFTW_EXECUTABLES[@]}"; do
+  echo "Submitting job for executable: $EXE"
+
+  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
+    $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP
+done
