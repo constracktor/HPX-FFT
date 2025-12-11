d4dde135f5cfe81d72ead0988d993e9a52638753
Heiko Häfner <heiko@familie-haefner.de>
2025-12-11 13:56:58 +0100

    kurzer Zwischenstand

---
+++ test/src/test_vector_2d.cpp
@@ -0,0 +1,30 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_test_macros.hpp>
+#include <iostream>
+#include "../../core/include/hpxfft/util/vector_2d.hpp"
+
+TEST_CASE("Vector 2D constant: Initialization", "[vector_2d][init]") {
+    hpxfft::util::vector_2d<double> vec(3, 3, 4.0);
+
+    REQUIRE(vec.n_row() == 3);
+    REQUIRE(vec.n_col() == 3);
+    REQUIRE(vec(0, 0) == 4.0);
+    REQUIRE(vec(2, 2) == 4.0);
+    REQUIRE(vec(1, 2) == 4.0);
+    REQUIRE(vec.size() == 9);
+}
+
+TEST_CASE("Vector 2D: Access Out of Range", "[vector_2d][exception]") {
+    hpxfft::util::vector_2d<double> vec(3, 3, 1.0);
+
+    REQUIRE_THROWS_AS(vec.at(3, 3), std::runtime_error);
+}
+
+TEST_CASE("Compare two Vector 2D instances", "[vector_2d][compare]") {
+    hpxfft::util::vector_2d<double> vec1(2, 2, 5.0);
+    hpxfft::util::vector_2d<double> vec2(2, 2, 5.0);
+    hpxfft::util::vector_2d<double> vec3(2, 2, 6.0);
+
+    REQUIRE(vec1 == vec2);
+    REQUIRE(!(vec1 == vec3));
+}
