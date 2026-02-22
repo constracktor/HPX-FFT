cfa9a9b9df5d8b0f64db346265eb6479e61d5792
constracktor <74077030+constracktor@users.noreply.github.com>
2026-02-22 10:31:46 -0600

    Adjust distributed benchmark for buran

--- benchmark/distributed_benchmark.sh
+++ benchmark/distributed_benchmark.sh
@@ -2,6 +2,7 @@
 # Benchmark script for shared memory 
 # $1: FFTW planning flag (estimate/measure)
 # $2: Collective (scatter/all_to_all)
+# $3: HPX Parcelport (tcp/mpi/lci)
 ################################################################################
 # Config
 ################################################################################
@@ -11,30 +12,21 @@ EXAMPLES_DIR=$TOP_DIR/examples
 # HPX-FFT implementations
 HPXFFT_EXECUTABLES=(
     "$EXAMPLES_DIR/hpxfft/build/hpxfft_distributed_loop"
-    #"$EXAMPLES_DIR/hpxfft/build/hpxfft_distributed_agas"
+    "$EXAMPLES_DIR/hpxfft/build/hpxfft_distributed_agas"
 )
 # FFTW implementations
 FFTW_EXECUTABLES=(
-    "$EXAMPLES_DIR/fftw/build/fftw_mpi_omp"
+    #"$EXAMPLES_DIR/fftw/build/fftw_mpi_omp"
     "$EXAMPLES_DIR/fftw/build/fftw_mpi_threads"
 )
 
 # Get current hostname
 HOSTNAME=$(hostname -s)
-if [[ "$HOSTNAME" == "ipvsmisc" ]]; then
-    echo "tbd."
-elif [[ "$HOSTNAME" == "rostam1" ]]; then
+if [[ "$HOSTNAME" == "rostam1" ]]; then
     PARTITION=buran
-    NODES_POW=3
+    NODES_POW=4
     THREADS=48
     module load gcc/14.2.0
-    spack env activate hpxfft_x86_buran
-elif [[ "$HOSTNAME" == "login1" ]]; then
-    PARTITION=short
-    NODES_POW=1
-    THREADS=48
-elif [[ "$HOSTNAME" == "simcl1" ]]; then
-    echo "tbd."
 else
     echo "Hostname is $HOSTNAME — no action taken."
     exit 1
@@ -46,7 +38,7 @@ FFTW_PLAN=$1
 # Check if FFTW_PLAN was provided
 if [ -z "$FFTW_PLAN" ]; then
     echo "Error: FFTW_PLAN parameter not set."
-    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all"
+    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all tcp/mpi/lci"
     exit 1
 fi
 # Set Collective
@@ -54,14 +46,22 @@ COLLECTIVE=$2
 # Check if COLLECITVE was provided
 if [ -z "$COLLECTIVE" ]; then
     echo "Error: COLLECTIVE parameter not set."
-    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all"
+    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all tcp/mpi/lci"
+    exit 1
+fi
+# Set HPX Parcelport
+PARCELPORT=$3
+# Check if COLLECITVE was provided
+if [ -z "$PARCELPORT" ]; then
+    echo "Error: Parcelport parameter not set."
+    echo "Usage: distributed_benchmark.sh estimate/measure scatter/all_to_all tcp/mpi/lci"
     exit 1
 fi
 
 ################################################################################
 # Run benchmarks
 ################################################################################
-RESULT_DIR=$TOP_DIR/distributed_benchmark_on_$HOSTNAME
+RESULT_DIR=$TOP_DIR/distributed_benchmark_on_$PARTITION
 SCRIPT_DIR=$TOP_DIR/benchmark/sbatch_scripts
 
 ################################################################################
@@ -72,7 +72,7 @@ mkdir -p $RESULT_DIR/strong_scaling/plans
 cd $RESULT_DIR/strong_scaling
 
 # Config
-LOOP=2
+LOOP=10
 BASE_SIZE=16384
 START_NODES_POW=0
 
@@ -81,9 +81,8 @@ RUN_SCRIPT="$SCRIPT_DIR/run_hpxfft_strong_distributed.sh"
 for EXE in "${HPXFFT_EXECUTABLES[@]}"; do
   echo "Submitting job for executable: $EXE"
 
-  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
-    $RUN_SCRIPT \
-    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP
+  $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP $PARTITION $PARCELPORT
 done
 
 # Loop over FFTW executables
@@ -92,9 +91,8 @@ RUN_SCRIPT="$SCRIPT_DIR/run_fftw_strong_distributed.sh"
 for EXE in "${FFTW_EXECUTABLES[@]}"; do
   echo "Submitting job for executable: $EXE"
 
-  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
-    $RUN_SCRIPT \
-    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP
+  $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP $PARTITION
 done
 
 ################################################################################
@@ -105,7 +103,7 @@ mkdir -p $RESULT_DIR/weak_scaling/plans
 cd $RESULT_DIR/weak_scaling
 
 # Config
-LOOP=2
+LOOP=10
 BASE_SIZE=512
 START_THREAD_POW=0
 
@@ -114,9 +112,8 @@ RUN_SCRIPT="$SCRIPT_DIR/run_hpxfft_weak_distributed.sh"
 for EXE in "${HPXFFT_EXECUTABLES[@]}"; do
   echo "Submitting job for executable: $EXE"
 
-  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
-    $RUN_SCRIPT \
-    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP
+  $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $COLLECTIVE $THREADS $LOOP $PARTITION $PARCELPORT
 done
 
 # Loop over FFTW executables
@@ -125,7 +122,6 @@ RUN_SCRIPT="$SCRIPT_DIR/run_fftw_weak_distributed.sh"
 for EXE in "${FFTW_EXECUTABLES[@]}"; do
   echo "Submitting job for executable: $EXE"
 
-  sbatch -p $PARTITION -N $NODES -n $NODES -c $THREADS \
-    $RUN_SCRIPT \
-    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP
+  $RUN_SCRIPT \
+    $EXE $FFTW_PLAN $START_NODES_POW $NODES_POW $BASE_SIZE $THREADS $LOOP $PARTITION
 done
--- benchmark/sbatch_scripts/run_hpxfft_weak_distributed.sh
+++ benchmark/sbatch_scripts/run_hpxfft_weak_distributed.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-#SBATCH --job-name=hpxfft_job          # Job name
-#SBATCH --output=hpxfft_job.log        # Standard output and error log
-#SBATCH --mail-type=NONE               # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=alexander.strack@ipvs.uni-stuttgart.de       # Where to send mail	
-#SBATCH --time=2:00:00                 # Time limit hrs:min:sec
-#SBATCH --exclusive                    # Exclusive ressource access
 
-# Benchmark script for distributed memory weak scaling
+# Benchmark script for distributed memory strong scaling
 # $1: Executable name
 # $2: FFTW planning flag (estimate/measure)
 # $3: Starting number of nodes
@@ -15,6 +9,8 @@
 # $6: HPX collective (scatter/all_to_all)
 # $7: Number of threads per node
 # $8: Number of runs
+# $9: Partition
+# $10: HPX Parcelport
 
 # Log Info
 pwd; hostname; date
@@ -24,24 +20,30 @@ POW_STOP=$4
 BASE_SIZE=$5
 THREADS=$7
 LOOP=$8
+PARTITION=$9
+PARCELPORT=${10}
 # Get run command
 OPTIONS=""
-COMMAND="srun -N $((2**POW_START)) -n $((2**POW_START)) -c $THREADS"
+COMMAND="srun --mpi=pmix -p $PARTITION --nodelist=$PARTITION[00-0$((2**POW_START - 1))] -N $((2**POW_START)) -n $((2**POW_START)) -c $THREADS"
 EXECUTABLE=$1
 ARGUMENTS="--nx=$BASE_SIZE --ny=$BASE_SIZE --plan=$2 --run=$6"
+PARCELPORTS="--hpx:ini=hpx.parcel.mpi.enable=0 --hpx:ini=hpx.parcel.tcp.enable=0 --hpx:ini=hpx.parcel.lci.enable=0 --hpx:ini=hpx.parcel.$PARCELPORT.enable=1"
 
-# Strong scaling loop from 2^pow_start to 2^pow_stop nodes
+# Weak scaling loop from 2^pow_start to 2^pow_stop nodes
+echo 'Submiting:' $COMMAND $EXECUTABLE $ARGUMENTS $PARCELPORTS
 HPX_COMMANDLINE_OPTIONS=$OPTIONS $COMMAND $EXECUTABLE $ARGUMENTS --header=true
 for (( j=1; j<$LOOP; j=j+1 ))
 do
+    echo 'Submiting:' $COMMAND $EXECUTABLE $ARGUMENTS $PARCELPORTS
     HPX_COMMANDLINE_OPTIONS=$OPTIONS $COMMAND $EXECUTABLE $ARGUMENTS
 done
 for (( i=2**($POW_START+1); i<=2**$POW_STOP; i=i*2 ))
 do
-    COMMAND="srun -N $i -n $i -c $THREADS"
+    COMMAND="srun --mpi=pmix -p $PARTITION --nodelist=$PARTITION[00-0$(($i - 1))] -N $i -n $i -c $THREADS"
     ARGUMENTS="--nx=$((BASE_SIZE*i)) --ny=$((BASE_SIZE*i)) --plan=$2"
     for (( j=0; j<$LOOP; j=j+1 ))
     do
+        echo 'Submiting:' $COMMAND $EXECUTABLE $ARGUMENTS $PARCELPORTS
         HPX_COMMANDLINE_OPTIONS=$OPTIONS $COMMAND $EXECUTABLE $ARGUMENTS
     done
 done
